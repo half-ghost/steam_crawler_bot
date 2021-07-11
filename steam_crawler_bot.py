089aed998eb1254952fd10934ba847aff4d4f3e3
half-ghost <55418764+half-ghost@users.noreply.github.com>
2021-07-11 17:17:44 +0800

    Update steam_crawler_bot.py

--- steam_crawler_bot.py
+++ steam_crawler_bot.py
@@ -25,6 +25,8 @@ def crawler(url_choose):
         os.mkdir(FILE_PATH)
     if os.path.exists(FILE_NAME(url_choose)):
         os.remove(FILE_NAME(url_choose))
+    with open(os.path.join(TAG_PATH, "tag2.json"), "r", encoding="utf-8")as f:
+        data = json.loads(f.read())
     get_request = get(url_choose).content.decode()
     soup = bs(get_request.replace(r"\n", "").replace(r"\t", "").replace(r"\r", "").replace("\\", ""), "lxml")
     row_list = soup.find_all(name = "a", class_ = "search_result_row")
@@ -32,6 +34,8 @@ def crawler(url_choose):
     price_list = []
     href_list = []
     img_list = []
+    rate_list = []
+    tag_list = []
     for row in row_list:
         soup_list = bs(str(row), "lxml")
         # 获取标题
@@ -43,9 +47,6 @@ def crawler(url_choose):
         # 获取缩略图链接
         img = re.findall(r"src=\"(.*?)\"", str(row))
         img_list.append(img[0])
-        # res = get(str(img[0]).replace("capsule_sm_120", "header")).content
-        # with open(os.path.join(IMG_PATH, str(row_list.index(row))) + ".jpg", 'wb') as f:
-        #     f.write(res)
         # 获取价格
         if str(soup_list.strike) == "None" :
             try:
@@ -59,8 +60,22 @@ def crawler(url_choose):
                 price_list.append("免费开玩")
         else:
             discount = re.findall(r"<br/>(.*?)  ", str(row))
-            price_list.append(soup_list.strike.string.replace(" ", "") + " 折扣价为" + str(discount[0]).replace(" ", ""))
-    
+            discount_percent = re.findall(r"<span>(.*?)</span>", str(row))
+            price_list.append(f'{soup_list.strike.string.replace(" ", "")} 折扣价：{str(discount[0]).replace(" ", "")}({discount_percent[0]})')
+        # 获取用户评价
+        rate = re.findall(r"data-tooltip-html=\"(.*?)\">", str(row))
+        try:
+            rate_list.append(rate[0].replace("&lt;br&gt", "").replace(" ", ""))
+        except:
+            rate_list.append("暂无用户评测")
+        # 获取标签
+        tag = soup_list.a["data-ds-tagids"].strip("[]").split(",")
+        tagk = ""
+        for k,v in data["tag_dict"].items():
+            for i in tag:
+                if i == v:
+                    tagk += k + ","
+        tag_list.append(tagk.strip(","))
     
     result_dict = {}
     for i in title_list:
@@ -69,6 +84,8 @@ def crawler(url_choose):
         result_dict["原价"] = price_list[index]
         result_dict["链接"] = href_list[index]
         result_dict["图片"] = img_list[index]
+        result_dict["评测"] = rate_list[index]
+        result_dict["标签"] = tag_list[index]
         with open(FILE_NAME(url_choose), "a", encoding="utf-8")as f:
             f.write(json.dumps(result_dict, ensure_ascii=False) + "\n")
 
@@ -78,14 +95,14 @@ def url_decide(tag, page):
     tag_name = ""
     tag_list = tag
     count = f"&start={(page-1)*50}&count=50"
-    with open(os.path.join(TAG_PATH, "tag.json"), "r", encoding="utf-8")as f:
-        for line in f.readlines():
-            for i in tag_list:
-                try:
-                    tag_search += json.loads(line)[i] + ","
-                    tag_name += i + ","
-                except:
-                    pass
+    with open(os.path.join(TAG_PATH, "tag2.json"), "r", encoding="utf-8")as f:
+        data = json.loads(f.read())
+        for i in tag_list:
+            try:
+                tag_search += data["tag_dict"][i] + ","
+                tag_name += i + ","
+            except:
+                pass
     tag_search_url = "https://store.steampowered.com/search/results/?query&force_infinite=1&filter=topsellers&snr=1_7_7_7000_7&infinite=1" + tag_search.strip(",") + count
     return tag_search_url,tag_name
 
