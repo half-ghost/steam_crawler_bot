de7cebe5a26ad582db4754bf3064f102c6a5d9d9
half-ghost <55418764+half-ghost@users.noreply.github.com>
2021-07-15 20:54:17 +0800

    Update steam_crawler_bot.py

--- steam_crawler_bot.py
+++ steam_crawler_bot.py
@@ -4,38 +4,16 @@ from requests import get
 import json
 import os
 
-FILE_PATH = os.path.join(os.path.dirname(__file__), "results")
 TAG_PATH = os.path.join(os.path.dirname(__file__))
-
-# 根据爬取的网址返回生成json文件的路径和命名
-def FILE_NAME(url_check):
-    if "Released_DESC" in url_check:
-        NAME = os.path.join(FILE_PATH, 'new_result.json')
-    elif "specials" in url_check:
-        NAME = os.path.join(FILE_PATH, 'specials_result.json')
-    elif "tags" in url_check:
-        NAME = os.path.join(FILE_PATH, f"search_tag={re.findall(r'tags=(.*?)&start', url_check)[0]}.json")
-    elif "term" in url_check:
-        NAME = os.path.join(FILE_PATH, f"search_term={url_check[url_check.find('term=')+5 :]}.json")
-    return NAME
 
 # 主爬虫程序
 def crawler(url_choose):
-    if not os.path.exists(FILE_PATH):
-        os.mkdir(FILE_PATH)
-    if os.path.exists(FILE_NAME(url_choose)):
-        os.remove(FILE_NAME(url_choose))
     with open(os.path.join(TAG_PATH, "tag.json"), "r", encoding="utf-8")as f:
         data = json.loads(f.read())
     get_request = get(url_choose).content.decode()
     soup = bs(get_request.replace(r"\n", "").replace(r"\t", "").replace(r"\r", "").replace("\\", ""), "lxml")
     row_list = soup.find_all(name = "a", class_ = "search_result_row")
-    title_list = []
-    price_list = []
-    href_list = []
-    img_list = []
-    rate_list = []
-    tag_list = []
+    title_list, price_list, href_list, img_list, rate_list, tag_list = [], [], [], [], [], []
     for row in row_list:
         soup_list = bs(str(row), "lxml")
         # 获取标题
@@ -77,17 +55,20 @@ def crawler(url_choose):
                     tagk += k + ","
         tag_list.append(tagk.strip(","))
     
-    result_dict = {}
+    mes_list = []
     for i in title_list:
         index = title_list.index(i)
-        result_dict["标题"] = title_list[index]
-        result_dict["原价"] = price_list[index]
-        result_dict["链接"] = href_list[index]
-        result_dict["图片"] = img_list[index]
-        result_dict["评测"] = rate_list[index]
-        result_dict["标签"] = tag_list[index]
-        with open(FILE_NAME(url_choose), "a", encoding="utf-8")as f:
-            f.write(json.dumps(result_dict, ensure_ascii=False) + "\n")
+        mes = f"[CQ:image,file={img_list[index]}]\n{title_list[index]}\n原价：{price_list[index]}\n链接:{href_list[index]}\n{rate_list[index]}\n用户标签：{tag_list[index]}"
+        data = {
+        "type": "node",
+        "data": {
+            "name": "sbeam机器人",
+            "uin": "2854196310",
+            "content":mes
+                }
+            }
+        mes_list.append(data)
+    return mes_list
 
 # 根据传入参数返回搜索页链接以及搜索的标签
 def url_decide(tag, page):
@@ -140,10 +121,13 @@ def hey_box(page):
             lowest_state = "是史低哦"
         else:
             lowest_state = "不是史低哦"
-        new_lowest = json_page["result"]["list"][i]["price"]["new_lowest"]
-        if new_lowest == 1:
-            newlowest = "好耶！是新史低！"
-        else:
+        try:
+            new_lowest = json_page["result"]["list"][i]["price"]["new_lowest"]
+            if new_lowest == 1:
+                newlowest = "好耶！是新史低！"
+            else:
+                newlowest = ""
+        except:
             newlowest = ""
         mes = f"[CQ:image,file={img}]\n{title}\n原价:¥{original} 当前价:¥{current} {lowest_state}\n链接:{href}\n{newlowest}\n".strip()
         data = {
