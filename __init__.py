e5e54c0e24fd7bd03e5d5dd475fe662e4e2bb2f3
half-ghost <55418764+half-ghost@users.noreply.github.com>
2021-07-15 20:53:47 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,58 +13,32 @@ help = '''
 '''.strip()
 
 from hoshino import Service
-from .steam_crawler_bot import crawler,FILE_NAME,url_decide,hey_box
-import json
-import os
+from .steam_crawler_bot import crawler,url_decide,hey_box
 
 sv = Service("stbot")
 url_new = "https://store.steampowered.com/search/results/?l=schinese&query&sort_by=Released_DESC&category1=998&os=win&infinite=1&start=0&count=50"
 url_specials = "https://store.steampowered.com/search/results/?l=schinese&query&sort_by=_ASC&category1=998&specials=1&os=win&filter=topsellers&start=0&count=50"
-FILE_PATH = os.path.dirname(__file__)
-
-def mes_creater(path):
-    mes_list = []
-    with open(path, "r", encoding="utf-8")as f:
-        for line in f.readlines():
-            result_dict = json.loads(line)
-            mes = f"[CQ:image,file={result_dict['图片']}]\n{result_dict['标题']}\n原价：{result_dict['原价']}\n链接:{result_dict['链接']}\n{result_dict['评测']}\n用户标签：{result_dict['标签']}"
-            data = {
-            "type": "node",
-            "data": {
-                "name": "sbeam机器人",
-                "uin": "2854196310",
-                "content":mes
-                    }
-                }
-            mes_list.append(data)
-    return mes_list
 
 # 匹配关键词发送相关信息，例：今日特惠，发送今日特惠信息，今日新品则发送新品信息
 @sv.on_prefix('今日')
 async def Gameinfo(bot, ev):
     model = ev.message.extract_plain_text().strip()
     if model == "新品":
-        open_file = FILE_NAME(url_new)
-        if not os.path.exists(open_file):
-            try:
-                crawler(url_new)
-            except Exception as e:
-                sv.logger.error(f"Error:{e}")
-                await bot.send(ev, "哦吼，出错了，请检查主机网络情况或者查看运行日志")
+        try:
+            mes_list = crawler(url_new)
+            await bot.send(ev, "正在生成合并消息，请稍等片刻！", at_sender=True)
+            await bot.send_group_forward_msg(group_id=ev['group_id'], messages=mes_list)
+        except Exception as e:
+            sv.logger.error(f"Error:{e}")
+            await bot.send(ev, "哦吼，出错了，请检查主机网络情况、查看运行日志或者再试一遍")
     elif model == "特惠":
-        open_file = FILE_NAME(url_specials)
-        if not os.path.exists(open_file):
-            try:
-                crawler(url_specials)
-            except Exception as e:
-                sv.logger.error(f"Error:{e}")
-                await bot.send(ev, "哦吼，出错了，请检查主机网络情况或者查看运行日志")
-    try:
-        mes_list = mes_creater(open_file)
-        await bot.send(ev, "正在生成合并消息，请稍等片刻！", at_sender=True)
-        await bot.send_group_forward_msg(group_id=ev['group_id'], messages=mes_list)
-    except:
-        pass
+        try:
+            mes_list = crawler(url_specials)
+            await bot.send(ev, "正在生成合并消息，请稍等片刻！", at_sender=True)
+            await bot.send_group_forward_msg(group_id=ev['group_id'], messages=mes_list)
+        except Exception as e:
+            sv.logger.error(f"Error:{e}")
+            await bot.send(ev, "哦吼，出错了，请检查主机网络情况、查看运行日志或者再试一遍")
 
 # 后接格式：页数(阿拉伯数字) 标签1 标签2，例：st搜标签1 动作 射击
 @sv.on_prefix(('st搜标签','St搜标签','ST搜标签'))
@@ -76,33 +50,29 @@ async def search_tag(bot, ev):
         pass
     else:
         try:
-            crawler(url[0])
+            mes_list = crawler(url[0])
             await bot.send(ev, "正在搜索并生成合并消息中，请稍等片刻！", at_sender=True)
-            mes_list = mes_creater(FILE_NAME(url[0]))
             await bot.send(ev, f"标签{url[1].strip(',')}搜索结果如下:")
             await bot.send_group_forward_msg(group_id=ev['group_id'], messages=mes_list)
         except Exception as e:
             sv.logger.error(f"Error:{e}")
-            await bot.send(ev, "哦吼，出错了，请检查主机网络情况或者查看运行日志")
+            await bot.send(ev, "哦吼，出错了，请检查主机网络情况、查看运行日志或者再试一遍")
         
-
 # 后接游戏名，例：st搜游戏美少女万华镜
 @sv.on_prefix(('st搜游戏','St搜游戏','ST搜游戏'))
 async def search_term(bot, ev):
     term = ev.message.extract_plain_text().strip()
     url_term = "https://store.steampowered.com/search/results/?l=schinese&query&start=0&count=50&dynamic_data=&sort_by=_ASC&snr=1_7_7_151_7&infinite=1&term=" + term
     try:
-        crawler(url_term)
+        mes_list =crawler(url_term)
+        if len(mes_list) == 0:
+            await bot.send(ev, "无搜索结果")
+        else:
+            await bot.send(ev, f"游戏{term}搜索结果如下:", at_sender=True)
+            await bot.send_group_forward_msg(group_id=ev['group_id'], messages=mes_list)
     except Exception as e:
         sv.logger.error(f"Error:{e}")
-        await bot.send(ev, "哦吼，出错了，请检查主机网络情况或者查看运行日志")
-    mes_list = []
-    try:
-        mes_list = mes_creater(FILE_NAME(url_term))
-        await bot.send(ev, f"游戏{term}搜索结果如下:", at_sender=True)
-        await bot.send_group_forward_msg(group_id=ev['group_id'], messages=mes_list)
-    except:
-        await bot.send(ev, "无搜索结果")
+        await bot.send(ev, "哦吼，出错了，请检查主机网络情况、查看运行日志或者再试一遍")
 
 # 此为小黑盒查询默认情况，默认抓取第一页(每页30条信息)
 @sv.on_fullmatch('小黑盒查询')
@@ -113,7 +83,7 @@ async def heybox(bot, ev):
         await bot.send_group_forward_msg(group_id=ev['group_id'], messages=mes_list)
     except Exception as e:
         sv.logger.error(f"Error:{e}")
-        await bot.send(ev, "哦吼，出错了，请检查主机网络情况或者查看运行日志")
+        await bot.send(ev, "哦吼，出错了，请检查主机网络情况、查看运行日志或者再试一遍")
 
 # 后接页数(阿拉伯数字)，例：小黑盒查询页1
 @sv.on_prefix('小黑盒查询页')
@@ -125,20 +95,4 @@ async def heybox(bot, ev):
         await bot.send_group_forward_msg(group_id=ev['group_id'], messages=mes_list)
     except Exception as e:
         sv.logger.error(f"Error:{e}")
-        await bot.send(ev, "哦吼，出错了，请检查主机网络情况或者查看运行日志")
-    
-# 每半小时清理缓存的搜索结果
-@sv.scheduled_job('cron', hour='*', minute='30')
-async def cycle():
-    try:
-        for files in os.walk(FILE_PATH):
-            for i in files[2]:
-                if "search" in i:
-                    name = os.path.join(FILE_PATH, i)
-                    os.remove(name)
-    except:
-        pass
-
-@sv.on_fullmatch('st机器人帮助')
-async def bot_help(bot, ev):
-    await bot.send(ev, help)
+        await bot.send(ev, "哦吼，出错了，请检查主机网络情况、查看运行日志或者再试一遍")
